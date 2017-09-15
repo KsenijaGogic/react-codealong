9712da66b59994a4e72b51b63e103af1a3949e7a
Ksenija Gogic <ksenijagogic@gmail.com>
2017-09-15 11:50:18 -0400

    Added example colours collection

---
+++ src/exampleColors.js
@@ -0,0 +1,52 @@
+export default [
+  {
+    color: 'Primrose Yellow',
+    code: '13-0755',
+    hex: '#F6D258'
+  },
+  {
+    color: 'Pale Dogwood',
+    code: '13-1404',
+    hex: '#EFCEC5'
+  },
+  {
+    color: 'Hazelnut',
+    code: '14-1315',
+    hex: '#D1AF94'
+  },
+  {
+    color: 'Island Paradise',
+    code: '14-4620',
+    hex: '#97D5E0'
+  },
+  {
+    color: 'Greenery',
+    code: '15-0343',
+    hex: '#88B14B'
+  },
+  {
+    color: 'Flame',
+    code: '17-1462',
+    hex: '#EF562D'
+  },
+  {
+    color: 'Pink Yarrow',
+    code: '13-1404',
+    hex: '#D13076'
+  },
+  {
+    color: 'Niagara',
+    code: '17-4123',
+    hex: '#5587A2'
+  },
+  {
+    color: 'Kale',
+    code: '18-0107',
+    hex: '#5C7148'
+  },
+  {
+    color: 'Lapis Blue',
+    code: '19-4045',
+    hex: '#0C4C8A'
+  }
+]
